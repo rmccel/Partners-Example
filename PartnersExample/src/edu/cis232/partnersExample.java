616f1e7001950ec43bc7d7921a3dc190797e8ffb
SK207_26 <SK207_26@K207_26.carrollcc.edu>
2016-09-19 20:06:46 -0400

    lul

--- PartnersExample/src/edu/cis232/partnersExample.java
+++ PartnersExample/src/edu/cis232/partnersExample.java
@@ -3,7 +3,7 @@ package edu.cis232;
 public class partnersExample {
 
 	public static void main(String[] args) {
-		System.out.println("I like the number 7");
+		System.out.println("SWEET MOTHER OF GOD THIS IS A BRANCH");
 		System.out.println("Goodbye Earth");
 	}
 
