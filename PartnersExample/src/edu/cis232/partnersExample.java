93cddcf96fe15b844c6d0bb0c3dc203ae5085850
SK207_26 <SK207_26@K207_26.carrollcc.edu>
2016-09-19 20:09:23 -0400

    lulul

--- PartnersExample/src/edu/cis232/partnersExample.java
+++ PartnersExample/src/edu/cis232/partnersExample.java
@@ -5,6 +5,7 @@ public class partnersExample {
 	public static void main(String[] args) {
 		System.out.println("SWEET MOTHER OF GOD THIS IS A BRANCH");
 		System.out.println("Goodbye Earth");
+		System.out.println("Goodbye Earth2");
 	}
 
 }
